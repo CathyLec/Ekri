7dd32a69bfcc89398eac3a501d79807929defa5c
CathyLec <cathyleru97@gmail.com>
2018-09-04 01:19:48 -0500

    mejoras nav

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
-$(function(){
+function start(){
+    var itemMore= document.getElementById(itemMore);
 
-});
+    itemMore.addEventListener("click",function(){
+        moveTextArea();
+    });
+}
+
+function moveTextArea(){
+    var textzone= document.getElementById(textzone);
+    textzone.style.marginLeft=20;
+    textzone.style.height=70;
+
+}
+
 
